0242c194384308fe1ba8bc08268e9cdbaa05a837
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-01-19 23:29:11 +0600

    Added the rest linter rules

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,9 +1,23 @@
 export enum RuleKeys {
-    'warningTextSizesShouldBeEqual'
+    'warningTextSizesShouldBeEqual',
+    'warningInvalidButtonSize',
+    'warningInvalidButtonPosition',
+    'warningInvalidPlaceholderSize',
+    'textSeveralH1',
+    'textInvalidH2Position',
+    'textInvalidH3Position',
+    'gridTooMuchMarketingBlocks',
 }
 
 export const ruleKeysResolveMap: { [key: string]: RuleKeys } = {
     'WARNING.TEXT_SIZES_SHOULD_BE_EQUAL': RuleKeys.warningTextSizesShouldBeEqual,
+    'WARNING.INVALID_BUTTON_SIZE': RuleKeys.warningInvalidButtonSize,
+    'WARNING.INVALID_BUTTON_POSITION': RuleKeys.warningInvalidButtonPosition,
+    'WARNING.INVALID_PLACEHOLDER_SIZE': RuleKeys.warningInvalidPlaceholderSize,
+    'TEXT.SEVERAL_H1': RuleKeys.textSeveralH1,
+    'TEXT.INVALID_H2_POSITION': RuleKeys.textInvalidH2Position,
+    'TEXT.INVALID_H3_POSITION': RuleKeys.textInvalidH3Position,
+    'GRID.TOO_MUCH_MARKETING_BLOCKS': RuleKeys.gridTooMuchMarketingBlocks,
 };
 
 export enum Severity {
